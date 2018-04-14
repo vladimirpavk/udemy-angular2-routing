102eb43728072c6a71047fbd49b2b4307a1e67d1
Korisnik <vladimirpavk@gmail.com>
2018-04-15 00:03:56 +0200

    error page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 import { AppRoutingModule } from './app.routing.module'
 
@@ -26,7 +27,8 @@ import { AppRoutingModule } from './app.routing.module'
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -10,7 +10,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const appRoutes: Route[] = [
     // { path:'', redirectTo: '/home', pathMatch: 'full' },
@@ -22,7 +22,8 @@ export const appRoutes: Route[] = [
         { path: ':id', component: ServerComponent },
         { path: ':id/edit', component: EditServerComponent }]
      },
-     { path: 'not-found', component: PageNotFoundComponent },
+     //{ path: 'not-found', component: PageNotFoundComponent },
+     { path:'not-found', component: ErrorPageComponent, data: {message:"Some problem occured"} },
      { path: '**', redirectTo: '/not-found'}
      ];
 
